b6bbaef791eca439d80fe45b84377a216deeabe9
nefilmjp <136662366+nefilmjp@users.noreply.github.com>
2023-07-08 15:51:39 +0900

    Fix global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={roboto.className}>
+      <body className={roboto.className} style={{ minHeight: '100dvh' }}>
         <Providers>{children}</Providers>
       </body>
     </html>
