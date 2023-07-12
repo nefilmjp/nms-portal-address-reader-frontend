282c09dc28cc818ee169c8e97fdf428166692fca
nefilmjp <136662366+nefilmjp@users.noreply.github.com>
2023-07-12 19:48:54 +0900

    First release

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# No Man's Sky Portal Address Reader Web Application
+# No Man's Sky Portal Address Reader
+
+Recognize portal glyphs in the screenshot and convert them to hex, decimal, and image.
 
 Available at [https://nmspar.vercell.app](https://nmspar.vercell.app)
 
 ## Contributing
 
-- Bug report
-    - Issues -> Bug report
 - Misrecognition report
     - Issues -> Misrecognition report
+- Bug report
+    - Issues -> Bug report
 - Feature request
     - Discussion -> Ideas
 - Pull Requests are accepted
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,16 +4,14 @@ export const Header = () => {
   return (
     <>
       <Heading className='ff-geo' fontSize='2xl'>
-        <span style={{ whiteSpace: 'nowrap' }}>No Man&#39;s Sky</span>{' '}
-        <span style={{ whiteSpace: 'nowrap' }}>Portal Address Reader (RC)</span>
+        <Text as='span' whiteSpace='nowrap'>
+          No Man&#39;s Sky
+        </Text>{' '}
+        <Text as='span' whiteSpace='nowrap'>
+          Portal Address Reader
+        </Text>
       </Heading>
-      <Text mt='4'>
-        The portal address reader in the screenshot.
-        <br />
-        Testing is in progress.
-        <br />
-        Do not use it frequently within a short time.
-      </Text>
+      <Text mt='4'>Recognize portal glyphs in the screenshot.</Text>
       <Box mt='4' borderRadius='lg' borderWidth='1px' p='3' fontSize='sm'>
         <Text ml='1em' textIndent='-1em'>
           Supported formats:
